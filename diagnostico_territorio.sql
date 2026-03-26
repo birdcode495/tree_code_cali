341aefc143a750dc0dd3d7445b08db6737fecf55
BirdCode888 <birdcode.cali@gmail.com>
2026-03-26 13:44:15 -0500

    Construccion de tablas de nombres de especies por comuna

--- diagnostico_territorio.sql
+++ diagnostico_territorio.sql
@@ -5,3 +5,11 @@ FROM comunas, censo_arboreo
 WHERE ST_Intersects(comunas.geom, censo_arboreo.geom)
 GROUP BY comuna ORDER BY riqueza_especies DESC;
 
+---- Construccion de tabla de especies por comunas
+
+SELECT caespecie, COUNT(DISTINCT censo_arboreo.id) AS reg
+FROM censo_arboreo, comunas
+WHERE ST_Intersects(censo_arboreo.geom, comunas.geom) AND comuna = 1
+GROUP BY caespecie ORDER BY reg DESC;
+
+
