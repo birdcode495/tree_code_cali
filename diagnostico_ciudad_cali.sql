0168858ac5979d007a3d2d5af06dbdbac8778344
BirdCode888 <birdcode.cali@gmail.com>
2026-04-01 14:58:29 -0500

    Desarrollo para enlazar especies del Censo Arboreo con GBIF API

--- diagnostico_ciudad_cali.sql
+++ diagnostico_ciudad_cali.sql
@@ -1,3 +1,11 @@
+---- Diagnostico de especies por abundancia en la ciudad de Cali
+
 SELECT caespecie AS especie, COUNT(DISTINCT id) AS registros
 FROM censo_arboreo
 GROUP BY especie ORDER BY registros DESC;
+
+---- Tabla para matching con GBIF API
+
+SELECT DISTINCT caespecie AS scientificName
+FROM censo_arboreo;
+
