aa9d5f03b3ac87e59b128f7318b0a43097fbe8bb
BirdCode888 <birdcode.cali@gmail.com>
2026-04-01 18:55:58 -0500

    Desarrollo para enlazar especies del Censo Arboreo con GBIF API parte 2

--- diagnostico_ciudad_cali.sql
+++ diagnostico_ciudad_cali.sql
@@ -9,3 +9,23 @@ GROUP BY especie ORDER BY registros DESC;
 SELECT DISTINCT caespecie AS scientificName
 FROM censo_arboreo;
 
+---- Creación de campo en tabla del Censo Arboreo para crear link a la API GBIF por especie
+
+ALTER TABLE censo_arboreo ADD COLUMN gbif_key varchar(30), ADD COLUMN gbif_link varchar(150);
+
+UPDATE censo_arboreo SET gbif_key = matching_gbif.key
+FROM matching_gbif
+WHERE censo_arboreo.caespecie = matching_gbif.verbatimscientificname;
+
+UPDATE censo_arboreo SET gbif_link = 'https://www.gbif.org/es/species/' || gbif_key;
+
+---- Construccion de tabla de especies del Censo Arboreo para la investigacion con API GBIF
+
+SELECT caespecie AS nombre_cientifico, canomcom AS nombre_comun, cafamilia AS familia,
+COUNT(DISTINCT id) AS registros, gbif_link AS gbif
+FROM censo_arboreo
+GROUP BY nombre_cientifico, nombre_comun, familia, gbif
+ORDER BY registros DESC;
+
+
+
